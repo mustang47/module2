2b2af4f15018657978aec0748fb3cc1f40f40240
Ekaterina Ogorodnikova <info@gotip.ru>
2018-02-13 01:22:02 +0300

    new uploadFolders and files

---
+++ app.js
@@ -0,0 +1,81 @@
+(function(){
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
+    function ToBuyController(ShoppingListCheckOffService){
+        var buyList = this;
+
+        buyList.items =  ShoppingListCheckOffService.getItems();
+        buyList.BuyItem = function(itemIndex){
+            ShoppingListCheckOffService.BuyItem(itemIndex);
+        };
+
+        buyList.checkError = function(){
+            return ShoppingListCheckOffService.checkError();
+        }
+
+
+    }
+
+    function AlreadyBoughtController(ShoppingListCheckOffService){
+        var boughtList = this;
+        boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+        boughtList.checkError2 = function(){
+           return ShoppingListCheckOffService.checkError2();
+        }
+    }
+
+    function ShoppingListCheckOffService(){
+        var service = this;
+        var itemsToBuy =[
+            { name: "Bread",  quantity: "3 pcs" },
+            { name: "Olive oil",  quantity: "1 bottle" },
+            { name: "Potato", quantity: "1 bag" },
+            { name: "Cheese", quantity: "2 pcs" }
+        ];
+        var boughtItems = [];
+    
+        service.getItems = function () {
+          
+            return itemsToBuy;
+          };
+        
+        service.getBoughtItems = function(){
+           return boughtItems;
+           
+        };
+        
+        service.BuyItem = function(itemIndex){
+           
+            var item = itemsToBuy[itemIndex];
+            boughtItems.push(item);
+            itemsToBuy.splice(itemIndex, 1);
+            
+
+        };
+
+        service.checkError = function(){
+            if(itemsToBuy.length){
+                return 0;
+            } else {
+                return 1;
+            }
+        };
+
+        service.checkError2 = function(){
+            if(boughtItems.length){
+                return 0;
+            } else {
+                return 1;
+            }
+        };
+    }
+
+})();
